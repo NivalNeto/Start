6bd29eb1e570f20b387ee91828c283f71c218521
Nival Balduino Neto <55193726+NivalNeto@users.noreply.github.com>
2019-09-24 20:15:26 -0300

    Update 3°lista leandro.c

--- 3°lista leandro.c
+++ 3°lista leandro.c
@@ -18,25 +18,6 @@ int main()
 	printf("Seu nome possui %d caracteres", i);	
 	//OS ESPAÇOS SÃO CONTADOS !
 }
-	//Questão 2
-	char s[20];
-	int i = 0;
-	int contador = 0;
 
-	printf("Escreva seu texto: ");
-	gets(s);
-	for(i =0; s[i] != '\0' && i <sizeof(s); i++){
-			if(s[i] == 'a' || s[i] == 'a' ||
-			s[i] =='e' || s[i] == 'E' ||
-			s[i] =='i' || s[i] == 'I' ||
-			s[i] =='o' || s[i] == 'O' ||
-			s[i] =='u' || s[i] == 'u') 
-		
-			contador++;
-			}	
-	printf("A quantidade de vogais são %d", contador);
-		
-	}
-	
 
 	

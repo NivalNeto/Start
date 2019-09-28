75890229957d7aade86a84e3e815f04ef2d77e0e
Nival Balduino Neto <55193726+NivalNeto@users.noreply.github.com>
2019-09-28 17:46:37 -0300

    Create Questão 4 da 3°lista leandro.c

---
+++ Questão 4 da 3°lista leandro.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main ()
+{
+	char texto1[50];
+	char texto2[50];
+    int i;
+		
+		printf("Digite o primeiro texto:\n ");
+		gets(texto1);
+		printf("Digite outro texto para concatenar:\n ");
+		gets(texto2);
+		
+		
+		printf("%s %s", texto1,texto2);
+		
+		return 0;
+		
+	
+		
+}
